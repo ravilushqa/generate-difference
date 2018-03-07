d65b1536ab24a524555fdc2b2b38234b51d08ba3
ravilushqa <ravil@sknt.ru>
2018-03-08 01:06:20 +0300

    added include docopt for global install

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,10 +1,13 @@
 <?php
-require_once(__DIR__.'/../vendor/docopt/docopt/src/docopt.php');
 
 $autoloadPath1 = __DIR__ . '/../../../autoload.php';
 $autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
+$docOptPath1 = __DIR__.'/../../../docopt/docopt/src/docopt.php';
+$docOptPath2 = __DIR__.'/../vendor/docopt/docopt/src/docopt.php';
 if (file_exists($autoloadPath1)) {
     include_once $autoloadPath1;
+    include_once $docOptPath1;
 } else {
     include_once $autoloadPath2;
+    include_once $docOptPath2;
 }
